1b4a470d578f74750e85e041a388a72dbf066267
tmckee <tmckee@falabella.cl>
2024-03-29 21:20:14 -0300

    works on mobile, sucks shit on pc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,26 +3,30 @@ import Draggable from "react-draggable";
 import { CompassBackground } from "@/components/CompassBackground";
 
 export default function Home() {
-  const compassRef = useRef<HTMLDivElement>(null); // Create a ref for the compass
+  const compassRef = useRef<HTMLDivElement | null>(null);
   const [defaultPosition, setDefaultPosition] = useState({ x: 0, y: 0 });
+  const [isPositionCalculated, setIsPositionCalculated] = useState(false);
 
   useEffect(() => {
     if (compassRef.current) {
       const compass = compassRef.current.getBoundingClientRect();
       const divSize = 64;
       const centerX = (compass.width - divSize) / 2;
-      const centerY = (compass.height - divSize) / 2;
-      setDefaultPosition({ x: centerX, y: centerY });
+      const centerY = (compass.width - divSize) / 2;
+      setDefaultPosition({ x: centerX - 18, y: centerY - 18 });
+      setIsPositionCalculated(true);
     }
-  }, []); // Empty dependency array ensures this runs once after initial render
+  }, []);
 
   return (
     <CompassBackground ref={compassRef}>
-      <Draggable bounds="parent" defaultPosition={defaultPosition}>
-        <div className="cursor-pointer p-2 bg-black rounded shadow w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28 flex items-center justify-center text-white">
-          {defaultPosition.x.toString()}
-        </div>
-      </Draggable>
+      {isPositionCalculated && (
+        <Draggable bounds="parent" defaultPosition={defaultPosition}>
+          <div className="text-xs cursor-pointer p-2 bg-black rounded shadow w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 lg:w-28 lg:h-28 flex items-center justify-center text-white">
+            {`x_1: ${defaultPosition.x}, y_1: ${defaultPosition.y}`}
+          </div>
+        </Draggable>
+      )}
     </CompassBackground>
   );
 }
