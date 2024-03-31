914f66f77ad989777b2c70a2a6099241b9c13fb8
tmckee <tmckee@falabella.cl>
2024-03-31 19:58:52 -0300

    voting api and """"database""""

---
+++ src/pages/api/vote.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+let votes: any[]= [];
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse,
+  ) {
+  if (req.method === 'POST') {
+    const { x, y } = req.body;
+    votes.push({ x, y });
+    return res.status(201).json({ message: 'Vote added' });
+  } else if (req.method === 'GET') {
+    const average = votes.reduce(
+      (acc, vote) => {
+        acc.x += vote.x;
+        acc.y += vote.y;
+        return acc;
+      },
+      { x: 0, y: 0 }
+    );
+
+    if (votes.length > 0) {
+      average.x /= votes.length;
+      average.y /= votes.length;
+    }
+
+    return res.status(200).json(average);
+  } 
+}
