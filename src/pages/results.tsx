4e3afdfbf789ae33fda5bbfbd3097357c4078ee3
tomitomitomitomitomitomi <tomasmaqui@gmail.com>
2024-04-24 12:35:06 -0400

    actual work on results page for once

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { CompassBackground } from "@/components/CompassBackground";
+import Draggable from "react-draggable";
 
 export default function Results() {
+  const debug = true;
   const [average, setAverage] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const fetchAverage = async () => {
       const response = await fetch("/api/vote");
       const data = await response.json();
+      console.log(data);
       setAverage(data);
     };
 
@@ -16,12 +20,17 @@ export default function Results() {
     return () => clearInterval(intervalId);
   }, []);
 
-  return (
-    <div>
-      <h1 className="text-black">Average Vote Position</h1>
-      <p className="text-black">
-        X: {average.x.toFixed(2)}, Y: {average.y.toFixed(2)}
-      </p>
-    </div>
+  return debug ? (
+    <h1 className="text-xl  bg-black rounded shadow flex items-center justify-center text-white">
+      `x:{average.x}, y:{average.y}`
+    </h1>
+  ) : (
+    <CompassBackground>
+      <Draggable bounds="parent" position={average}>
+        <div className="text-xs cursor-pointer p-5 bg-black rounded shadow w-0 h-0 flex items-center justify-center text-white">
+          `x:{average.x}, y:{average.y}`
+        </div>
+      </Draggable>
+    </CompassBackground>
   );
 }
