a6949b01a0848266fefcfc742ddaca1eade7db80
tomitomitomitomitomitomi <tomasmaqui@gmail.com>
2024-04-26 20:50:51 -0400

    works but sucks

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -3,35 +3,69 @@ import { CompassBackground } from "@/components/CompassBackground";
 import Draggable from "react-draggable";
 
 export default function Results() {
-  const debug = false;
+  const compassPadding = 20;
   const compassRef = useRef<HTMLDivElement | null>(null);
-  const centerRef = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
-  const [average, setAverage] = useState({ x: 0, y: 0 });
+  const [average, setAverage] = useState<{ x: number; y: number }>({
+    x: 0,
+    y: 0,
+  });
+  const [hasInitialFetch, setHasInitialFetch] = useState(false);
 
   useEffect(() => {
-    const fetchAverage = async () => {
+    const fetchAverage = async (compassWidth: any) => {
       const response = await fetch("/api/votes");
-      const data = await response.json();
-      setAverage(data);
+      const data: { x: number; y: number } = await response.json();
+      if (!hasInitialFetch) {
+        if (data.x !== 0 || data.y !== 0) {
+          setAverage({ x: data.x * compassWidth, y: data.y * compassWidth });
+        }
+        setHasInitialFetch(true);
+      } else {
+        setAverage({ x: data.x * compassWidth, y: data.y * compassWidth });
+      }
     };
 
-    fetchAverage();
-    const intervalId = setInterval(fetchAverage, 5000);
+    const setupInterval = () => {
+      if (compassRef.current) {
+        const compassWidth =
+          compassRef.current.getBoundingClientRect().width - compassPadding;
+        if (!hasInitialFetch) {
+          setAverage({
+            x: (compassWidth - compassPadding) / 2,
+            y: (compassWidth - compassPadding) / 2,
+          });
+        }
+        fetchAverage(compassWidth);
+        const intervalId = setInterval(() => {
+          if (compassRef.current) {
+            fetchAverage(
+              compassRef.current.getBoundingClientRect().width - compassPadding
+            );
+          }
+        }, 5000);
+        return intervalId;
+      }
+      return null;
+    };
 
-    return () => clearInterval(intervalId);
+    const intervalId = setupInterval();
+
+    return () => {
+      if (intervalId) clearInterval(intervalId);
+    };
   }, []);
 
   return (
     <>
-      <h1 className="text-xl  bg-black rounded shadow flex items-center justify-center text-white">
-        `x:{average.x}, y:{average.y}, compassWidth:
-        {compassRef.current?.clientWidth}`
+      <h1 className="text-xl bg-black rounded shadow flex items-center justify-center text-white">
+        x:{average.x}, y:{average.y}, compassWidth:{" "}
+        {compassRef.current?.clientWidth}
       </h1>
 
       <CompassBackground ref={compassRef}>
         <Draggable bounds="parent" position={average} disabled>
           <div className="text-xs cursor-pointer p-5 bg-black rounded shadow w-0 h-0 flex items-center justify-center text-white">
-            `x:{average.x}, y:{average.y}`
+            x:{average.x}, y:{average.y}
           </div>
         </Draggable>
       </CompassBackground>
