d75640889e5bcb259812ca2b306d598185766f70
tomitomitomitomitomitomi <tomasmaqui@gmail.com>
2024-04-26 20:18:54 -0400

    healthcheck is cuter now :3

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -7,7 +7,7 @@ type Data = {
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data | string>,
 ) {
-  res.status(200).json({ name: "hiiii" });
+  res.status(200).send('hiiii')
 }
