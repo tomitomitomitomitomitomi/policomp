8b79e340c1c156a26bdbe2a54fe931946095074d
tomitomitomitomitomitomi <tomasmaqui@gmail.com>
2024-04-26 20:19:24 -0400

    FAFO

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { CompassBackground } from "@/components/CompassBackground";
 import Draggable from "react-draggable";
 
 export default function Results() {
-  const debug = true;
+  const debug = false;
+  const compassRef = useRef<HTMLDivElement | null>(null);
+  const centerRef = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
   const [average, setAverage] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
@@ -19,17 +21,20 @@ export default function Results() {
     return () => clearInterval(intervalId);
   }, []);
 
-  return debug ? (
-    <h1 className="text-xl  bg-black rounded shadow flex items-center justify-center text-white">
-      `x:{average.x}, y:{average.y}`
-    </h1>
-  ) : (
-    <CompassBackground>
-      <Draggable bounds="parent" position={average}>
-        <div className="text-xs cursor-pointer p-5 bg-black rounded shadow w-0 h-0 flex items-center justify-center text-white">
-          `x:{average.x}, y:{average.y}`
-        </div>
-      </Draggable>
-    </CompassBackground>
+  return (
+    <>
+      <h1 className="text-xl  bg-black rounded shadow flex items-center justify-center text-white">
+        `x:{average.x}, y:{average.y}, compassWidth:
+        {compassRef.current?.clientWidth}`
+      </h1>
+
+      <CompassBackground ref={compassRef}>
+        <Draggable bounds="parent" position={average} disabled>
+          <div className="text-xs cursor-pointer p-5 bg-black rounded shadow w-0 h-0 flex items-center justify-center text-white">
+            `x:{average.x}, y:{average.y}`
+          </div>
+        </Draggable>
+      </CompassBackground>
+    </>
   );
 }
